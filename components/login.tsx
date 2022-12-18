3d2f3def03f315656501baf1d3b2928a8e605e1b
Veit Progl <veit-progl@t-online.de>
2022-12-18 20:48:51 +0100

    add handleSubmit to form

--- components/login.tsx
+++ components/login.tsx
@@ -1,8 +1,12 @@
 import styles from '../styles/LoginForm.module.scss'
 
 function LoginForm() {
+  function handleSubmit(){
+
+  }
+
   return (
-    <form className={styles.LoginForm}>
+    <form className={styles.LoginForm} onSubmit={handleSubmit}>
       <h1>Login Form</h1>
       {/* <input type="text" value={this.state.value} onChange={this.handleChange} />-c */}
     </form>
